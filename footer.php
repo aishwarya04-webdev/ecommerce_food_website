a6eddf612497c2113a42da3750a8355de77bed29
aishwarya04-webdev <62304845+aishwarya04-webdev@users.noreply.github.com>
2020-07-28 16:57:43 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
   <a href="#myPage" title="To Top">
     <span class="glyphicon glyphicon-chevron-up"></span>
   </a>
-  <p>Bootstrap Theme Made By <a href="https://www.w3schools.com" title="Visit w3schools">www.w3schools.com</a></p>
+  <p>COPYRIGHTS @<?PHP  ECHO Date('Y');   ?></p>
 </footer>
 
 <script>
